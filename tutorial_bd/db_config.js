98f5ce6d04bae72d8f173286a80fe265344f3b74
Francisco Angelim <frnangelim@gmail.com>
2017-04-24 16:39:17 -0300

    BD básico para apresentacao do workshop

---
+++ tutorial_bd/db_config.js
@@ -0,0 +1,24 @@
+var db_string = 'mongodb://127.0.0.1/';
+
+var mongoose = require('mongoose').connect(db_string);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Erro ao conectar no banco'))
+
+db.once('open', function() {
+
+	var userSchema = mongoose.Schema({
+
+		fullName: String,
+		email: String,
+		birthDate: String,
+		cpf: String,
+		phoneNumber: String,
+		adress: String,
+		role: String,
+		coach: String
+	});
+
+	exports.User = mongoose.model('User', userSchema);
+});
